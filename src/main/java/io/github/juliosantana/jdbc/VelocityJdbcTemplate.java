09410df0a4f888e3e1dabce36971b03c7c52dde7
Julio Santana <juliosantana07@gmail.com>
2024-07-19 12:11:06 -0300

    Incluir javadoc interface

--- src/main/java/io/github/juliosantana/jdbc/VelocityJdbcTemplate.java
+++ src/main/java/io/github/juliosantana/jdbc/VelocityJdbcTemplate.java
@@ -11,14 +11,49 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 public interface VelocityJdbcTemplate {
 
+	/**
+	 * @param <T>
+	 * @param sql
+	 * @param paramSource
+	 * @param rowMapper
+	 * @return
+	 * @throws DataAccessException
+	 */
 	<T> Optional<T> queryForObject(String sql, SqlParameterSource paramSource, RowMapper<T> rowMapper)
 			throws DataAccessException;
 	
+	/**
+	 * @param <T>
+	 * @param sql
+	 * @param paramSource
+	 * @param rowMapper
+	 * @return
+	 * @throws DataAccessException
+	 */
 	<T> List<T> query(String sql, SqlParameterSource paramSource, RowMapper<T> rowMapper) throws DataAccessException;
 	
+	/**
+	 * @param <T>
+	 * @param query
+	 * @param paramSource
+	 * @param pageable
+	 * @param rowMapper
+	 * @return
+	 * @throws DataAccessException
+	 */
 	<T> Page<T> query(String query, SqlParameterSource paramSource, Pageable pageable, RowMapper<T> rowMapper)
 			throws DataAccessException;
 
+	/**
+	 * @param <T>
+	 * @param query
+	 * @param queryCount
+	 * @param paramSource
+	 * @param pageable
+	 * @param rowMapper
+	 * @return
+	 * @throws DataAccessException
+	 */
 	<T> Page<T> query(String query, String queryCount, SqlParameterSource paramSource, Pageable pageable,
 			RowMapper<T> rowMapper) throws DataAccessException;
 
